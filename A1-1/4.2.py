05beb7e6efc50af1227f4520322389362e6db68d
Song0Hwi <iamsongyounghwi@gmail.com>
2026-08-28 19:27:32 +0900

    개발 환경, Git 저장소 설정 및 초기화

--- A1-1/4.2.py
+++ A1-1/4.2.py
@@ -2,6 +2,25 @@
 #  프로젝트 폴더를 만들고 로컬에서 init으로 저장소를 시작한다.
 
 #  원격 저장소를 연결하고 첫 커밋과 푸시를 수행한다. (add, commit, push)
+# PS C:\Users\user\Desktop\A1-1\AI> git add .
+# PS C:\Users\user\Desktop\A1-1\AI> git commit          
+# [main 1348665] B
+#  4 files changed, 67 insertions(+)
+#  create mode 100644 A1-1/.gitignore
+#  create mode 100644 A1-1/4.1.py
+#  create mode 100644 A1-1/4.2.py
+#  create mode 100644 A1-1/README.md
+
+#push
+# PS C:\Users\user\Desktop\A1-1\AI> git push origin main
+# Enumerating objects: 8, done.
+# Counting objects: 100% (8/8), done.
+# Delta compression using up to 12 threads
+# Compressing objects: 100% (5/5), done.
+# Writing objects: 100% (6/6), 1.62 KiB | 832.00 KiB/s, done.
+# Total 6 (delta 1), reused 1 (delta 0), pack-reused 0 (from 0)
+# remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+# To https://github.com/Song0Hwi/AI.git
 
 #  .gitignore 파일을 생성하여 불필요한 파일을 제외한다.
 
